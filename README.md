92ffc812e8f1effe64b17e6a5c24665876d1a96c
Vedant Ralekar <ralekaru@gmai.com>
2025-01-29 00:49:40 +0530

    changed the files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LearningCode
+ Welcome to the coding journey "Hello World!!!"
+ <br>
+Author (Vedant)
---
+++ name.java
@@ -0,0 +1,8 @@
+package LearningCode;
+
+public class name{
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+    }
+
+} 
